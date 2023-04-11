81e2759f49d8922a08ae5567aa4077cd40277eb5
Mohit-P29 <71666621+Mohit-P29@users.noreply.github.com>
2023-04-11 17:08:33 -0400

    Removed cors single origin

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 
 const PORT = process.env.PORT || 5000
 //app.use(cors())
-app.use(cors({ credentials: true, origin: 'http://35.182.20.105:80' }))
+app.use(cors());
 
 const authMiddleware = auth({
   audience: 'https://dev-glrfz0b04ozteyjy.us.auth0.com/api/v2/',
