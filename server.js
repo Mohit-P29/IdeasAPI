f5386db4e3c221257a38dcb8f26a3f5237cd004a
Mohit-P29 <71666621+Mohit-P29@users.noreply.github.com>
2023-04-19 01:38:31 -0400

    Removed wildcard from server cors origin

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 const PORT = process.env.PORT || 5000
 
 //app.use(cors())
-app.use(cors({credentials: true, origin: '*', methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'] }));
+app.use(cors({credentials: true, methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'] }));
 
 
 app.get('/', (req, res) => {
